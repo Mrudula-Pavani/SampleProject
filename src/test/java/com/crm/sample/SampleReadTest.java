815589de72d2ed3a11618d5e7de9e11c3c4a43fb
mrudu <mrudu@LAPTOP-PF1SH7SD>
2021-12-13 15:08:38 +0530

    gitjenkin

---
+++ src/test/java/com/crm/sample/SampleReadTest.java
@@ -0,0 +1,15 @@
+package com.crm.sample;
+
+import org.testng.annotations.Test;
+
+public class SampleReadTest {
+	@Test
+	public void sampleReadTest() {
+		String url = System.getProperty("url");
+		String browser= System.getProperty("browser");
+		System.out.println(url);
+		System.out.println(browser);
+		
+	}
+
+}
